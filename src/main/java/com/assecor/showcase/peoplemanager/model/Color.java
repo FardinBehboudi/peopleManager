c8240982aece9d745e5031c213f90935d10da2cb
Fardin Behboudi <f_behboudi@hotmail.com>
2019-10-14 18:30:39 +0200

    color enum updated

--- src/main/java/com/assecor/showcase/peoplemanager/model/Color.java
+++ src/main/java/com/assecor/showcase/peoplemanager/model/Color.java
@@ -3,13 +3,11 @@ package com.assecor.showcase.peoplemanager.model;
 public enum Color {
     BLUE(1),
     GREEN(2),
-    RED(3),
-    YELLOW(4),
-    BROWN(5),
-    WHITE(6),
-    BLACK(7),
-    PINK(8),
-    ORANGE(9);
+    PURPLE(3),
+    RED(4),
+    YELLOW(5),
+    TURQUOIS(6),
+    WHITE(7);
 
 
     Color(int colorNumber) {
